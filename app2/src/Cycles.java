0fc2532107854cffd0a9be59e53defa160079543
Kenji <159823813+kenjle@users.noreply.github.com>
2024-06-14 14:08:07 -0400

    Update Cycles.java

--- app2/src/Cycles.java
+++ app2/src/Cycles.java
@@ -1,14 +1,18 @@
-import java.awt.event.WindowAdapter;
-import javax.swing.*;
-import java.awt.FlowLayout;
-import java.io.IOException;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.WindowAdapter;
+import java.io.IOException;
+import javax.swing.*;
 
 public class Cycles extends WindowAdapter{
+    
     JFrame f;
     // Method for starting the game
+    
     public void play(String[] args) throws IOException{
+
 
 
 
@@ -29,38 +33,45 @@ public class Cycles extends WindowAdapter{
 
     {  
         ImageIcon img = new ImageIcon("src/imagery/crescent-moon-icon-isolated-on-transparent-background-free-png.png");
-
-
-
-
-            if (answer1 < 1) {
-                message = "the total amount of days you going are " + answer1;
-            }
-            else if(answer1 > 1){
-                
-            message = "the total amount of days you going are " + answer1;
-            message1 = "the total amount of nights you going are " + night;
-    
-            }
+        
         f=new JFrame();
         JFrame frame = new JFrame("Cycles");  
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
         JLabel label = new JLabel();
-        label.setText("<html><body width='%1s'><h1>Results</h1>"
-        + "<p>" + message
-        + "<p>" + message1);
-        panel.add(label);   
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().setBackground(Color.darkGray);
+        label.setForeground(Color.WHITE);
+        panel.setBackground(Color.darkGray);
+        panel.add(label);  
         label.setVisible(true);
         frame.add(panel); 
         frame.pack();
         frame.setPreferredSize(new Dimension(1080, 480));
+    
+        frame.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.WHITE));
+        if (answer1 < 1) {
+            message = "Should take you at least under a day";
+            label.setText("<html><body width='%1s'><h1>Results</h1>"
+            + "<p>" + message);
+        }
+
+        else if(answer1 > 1){
+                
+            message = "the total amount of days you going are " + answer1;
+            message1 = "the total amount of nights you going are " + night;
+
+            label.setText("<html><body width='%1s'><h1>Results</h1>"
+            + "<p>" + message
+            + "<p>" + message1);
+        }
+        frame.add(label, BorderLayout.CENTER);
         frame.setVisible(true); 
         frame.setLocationRelativeTo(null);  
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         frame.setIconImage(img.getImage());
-        frame.add(new JLabel(new ImageIcon("src/imagery/crescent-moon-icon-isolated-on-transparent-background-free-png.png")));
-        frame.getContentPane().setBackground(Color.BLUE);
+
+
     
 
         }
